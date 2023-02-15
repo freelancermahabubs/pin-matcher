44941ba7043c61647cfbd257385d48ad915e269f
MAHABUB <info.freelancermahabub@gmail.com>
2023-02-15 19:32:49 +0600

    first commit

---
+++ js/m.js
@@ -0,0 +1,68 @@
+function getPin(){
+  const pin = generatePin();
+  const pinString = pin + "";
+  if(pinString.length === 4){
+    return pin;
+  }
+  else{
+    return generatePin();
+  }
+};
+
+function generatePin(){
+  const random = Math.round (Math.random()*10000);
+  return random;
+};
+
+document.getElementById("generate-pin").addEventListener("click", function(){
+  const pin = getPin();
+
+  // display pin
+  const displayPinField = document.getElementById("display-pin");
+  displayPinField.value = pin;
+});
+
+document.getElementById("calculator").addEventListener("click", function(event){
+  
+  const number = event.target.innerText;
+  const typedNumberField = document.getElementById("type-numbers");
+  const previousTypedNumber = typedNumberField.value;
+
+
+  if(isNaN (number)){
+    if(number === "C"){
+      typedNumberField.value = "";
+    }
+    else if(number === "<"){
+      const digit = previousTypedNumber.split("");
+      digit.pop();
+      const remainingDigits = digit.join("");
+      typedNumberField.value = remainingDigits;
+    }
+  }
+  else{
+    const newTypedNumber = previousTypedNumber + number;
+    typedNumberField.value = newTypedNumber;
+  }
+});
+
+document.getElementById("btn-submit").addEventListener('click', function(){
+
+  const displayPinField = document.getElementById("display-pin");
+  const currentPin = displayPinField.value;
+
+  const typedNumberField = document.getElementById("type-numbers");
+  const typedNumber = typedNumberField.value;
+
+  const pinSuccessMessage = document.getElementById("pin-success");
+  const pinFailureMassage = document.getElementById("pin-failure");
+  
+  if(typedNumber === currentPin){
+    pinSuccessMessage.style.display = "block";
+    pinFailureMassage.style.display = "none";
+  }
+  else{
+    pinFailureMassage.style.display = "block";
+    pinSuccessMessage.style.display = "none";
+  }
+})
